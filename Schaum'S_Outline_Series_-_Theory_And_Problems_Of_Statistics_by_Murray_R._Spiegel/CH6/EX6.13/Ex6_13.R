aa902ff6fc44cdd2097cc483dbcf0519168c2bc2
sashi20 <gayatrirekha204@gmail.com>
2022-03-31 00:28:52 +0550

    Added(A)/Deleted(D) following file  D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH12/EX12.12.8/Ex12_12_8.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH13/EX13.13.17/Ex13_13_17.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH13/EX13.13.19/Ex13_13_19.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH13/EX13.13.23/Ex13_13_23.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH13/EX13.13.5/Ex13_13_5.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH14/EX14.14.24/Ex14_14_24.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH14/EX14.14.35/Ex14_14_35.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH17/EX17.17.25/Ex17_17_25.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH17/EX17.17.8/Ex17_17_8.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH3/EX3.3.29/Ex3_3_29.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH3/EX3.3.46/Ex3_3_46.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH4/EX4.4.5/Ex4_4_5.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH6/EX6.13/Ex6_13.R D  Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH6/EX6.8/Ex6_8.R M  latex/TEX M  latex/TEX_final.aux M  latex/TEX_final.log M  latex/TEX_final.out M  latex/TEX_final.tex M  latex/database_sort M  latex/log.txt

--- Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH6/EX6.13/Ex6_13.R
+++
@@ -1,6 +0,0 @@
-#PAGE=127
-a=c('a','b','c')
-l=length(a)
-n=2
-ans=(l*n)/factorial(n)
-cat(ans)
--- Schaum'S_Outline_Series_-_Theory_And_Problems_Of_Statistics_by_Murray_R._Spiegel/CH6/EX6.8/Ex6_8.R
+++
@@ -1,7 +0,0 @@
-#PAGE=124
-x=c(2:12)
-p=(x-1)/6^2
-p=round(p,3)
-ans=900*p
-ans=round(ans,0)
-cat(ans[4])
